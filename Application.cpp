e0556fc642421bea4987ba517033f1307633e673
Igor <igor-alexey@hotmail.com>
2021-10-20 00:24:50 -0300

    Update Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -1,6 +1,18 @@
 //
 #include "RRE3D/rre3d.h"
 
+
+/* These initial functions are declared by RRE3D */
+
+/* Called upon initialization */
+
+void Start()
+{
+
+}
+
+/* Update every frame */
+
 void Update()
 {
 
