09dbecb6e1d06f1694f382529d6f0a561dbdadbe
Igor <igor-alexey@hotmail.com>
2021-10-21 01:30:50 -0300

    window setub & image stb

--- source/RRE3D/main.cpp
+++ source/RRE3D/main.cpp
@@ -1,14 +1,38 @@
 #include "RRE3D/rre3d.h"
+#include "GLFW/glfw3.h"
 
-/* This is the true entry point of the application */ 
+/* This is the true entry point of the application */
 int main()
 {
-    /* Once everything is ready, call the first function */
+    /* The 3D renderer window of the application */
+    GLFWwindow* window;
+
+    if(!glfwInit())
+      return 1;
+
+    /* Use OpenGL 4.6 */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 800, "RRE3D", NULL, NULL);
+    if (window == nullptr){
+		   glfwTerminate();
+  		 return 1;
+	  }
+    glfwMakeContextCurrent(window);
+
+    /* Once everything is ready, call the first function just before the main loop */
     RRE3D::Start();
 
     /* The main loop, which keeps the application open */
-    while(true)
+    while(!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
         RRE3D::Update();
     }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
