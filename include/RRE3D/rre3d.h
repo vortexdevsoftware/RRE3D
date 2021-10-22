1aa7ee63a453e32f55004f66a174f8458a0b95ab
wTechnoo <augustooliveiramaciel@gmail.com>
2021-10-22 11:20:43 -0300

    Added Vector3 class

--- include/RRE3D/rre3d.h
+++ include/RRE3D/rre3d.h
@@ -7,6 +7,55 @@ namespace RRE3D {
     {
 
     };
+
+    class Object
+    {
+        Vector3 position;
+        Mesh mesh;
+        Vector3 scale;
+    };
+
+    class Mesh
+    {
+        Vector3* vertices;
+
+        /* Create constructor that receives array of v3's */
+    };
+
+    struct Vector3
+    {
+        float x, y, z;
+        Vector3(float xx, float yy, float zz){
+            x = xx;
+            y = yy;
+            z = zz;
+        }
+
+        float distance(Vector3 vector){
+            return sqrt(dot(vector));
+        }
+        float dot(Vector3 vector){
+            return x * vector.x + y * vector.y + z * vector.z;
+        }
+        float dot(Vector3 v1, Vector3 v2){
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }
+        float operator*(float scalar){
+            x *= scalar;
+            y *= scalar;
+            z *= scalar;
+        }
+        float operator+(float scalar){
+            x += scalar;
+            y += scalar;
+            z += scalar;
+        }
+        float operator+(Vector3 vector){
+            x += vector.x;
+            y += vector.y;
+            z += vector.z;
+        }
+    };
     
     /* Functions */
 
