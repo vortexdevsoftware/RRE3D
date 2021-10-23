8975cac86805bbb7180e1e0a92e6bc4980ab9662
wTechnoo <augustooliveiramaciel@gmail.com>
2021-10-23 01:09:06 -0300

    Update shaders.h

--- include/RRE3D/shaders.h
+++ include/RRE3D/shaders.h
@@ -1,29 +1,33 @@
 #ifndef SHADERS_CLASS_H
 #define SHADERS_CLASS_H
 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 class Shader
 {
     GLuint shaderProgram;
-    VertexShader vertexShader;
-    FragmentShader fragmentShader;
 
-    Shader(VertexShader vShader, FragmentShader fShader)
+    Shader(const char *vShader, const char *fShader)
     {
+        GLuint vertex;
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShader, NULL);
+        glCompileShader(vertex);
+
+        GLuint frag;
+        frag = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(frag, 1, &fShader, NULL);
+        glCompileShader(frag);
+
         shaderProgram = glCreateProgram();
-        vertexShader = vShader;
-        fragmentShader = fShader;
+        glAttachShader(shaderProgram, vertex);
+	    glAttachShader(shaderProgram, frag);
+	    glLinkProgram(shaderProgram);
+
+        glDeleteShader(vertex);
+	    glDeleteShader(frag);
     }
 };
 
-class VertexShader
-{
-
-};
-
-class FragmentShader
-{
-
-};
-
 #endif
-#include "GLFW/glfw3.h"
