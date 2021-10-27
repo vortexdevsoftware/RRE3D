03ade904c9e2ba3d4054f09b95f19c5c1e48c4b2
Igor <igor-alexey@hotmail.com>
2021-10-27 10:03:46 -0300

    directory restructure

--- include/RRE3D/README.md
+++
@@ -1,2 +0,0 @@
-In this folder, is included the declarations that you most certainly need in order
-to call RRE3D functions along with the library.
--- source/RRE3D/main.cpp
+++ source/RRE3D/main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include "RRE3D/rre3d.h"
+#include "rre3d.h"
 #include "stb/stb_image.h"
 
 ///// PRE DEFINED VARIABLES ////
